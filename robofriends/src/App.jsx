156cd1c791fff02b8aef990ca60373b6b200a397
Alfredo <kmstry214@gmail.com>
2023-10-04 23:23:42 -0500

    search input working. Now working on styling.

--- robofriends/src/App.jsx
+++ robofriends/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from "react";
-import { Cards } from "./Cards";
-import "./App.css";
+import { Component } from "react";
+import { CardList } from "./CardList";
+import { robots } from "./robots";
+import { SearchBox } from "./SearchBox";
 
-function App() {
-  const [count, setCount] = useState(0);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
 
-  return (
-    <>
-      <Cards />
-    </>
-  );
+  onSearchChange = (e) => {
+    this.setState({ searchfield: e.target.value });
+  };
+
+  render() {
+    const filterRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox
+          searchChange={this.onSearchChange}
+          searchfield={this.searchfield}
+        />
+        <CardList robots={filterRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ robofriends/src/SearchBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const SearchBox = ({ searchfield, searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+        onChange={searchChange}
+        value={searchfield}
+      />
+    </div>
+  );
+};
